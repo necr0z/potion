ff78f6caed7c5b6b00b66ba3984e902178bcc4df
necr0z <necroz1998@gmail.com>
2024-03-14 18:44:17 +0800

    document list halfway

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
 
@@ -62,6 +63,20 @@ export const Item = ({
           <span className="text-xs">⌘</span>+ K
         </kbd>
       )}
+    </div>
+  );
+};
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div
+      style={{
+        paddingLeft: level ? `${level * 12 + 12}px` : "12px",
+      }}
+      className="flex gap-x-2 py-[3px]"
+    >
+      <Skeleton className="h-4 w-4" />
+      <Skeleton className="h-4 w-[30%]" />
     </div>
   );
 };
