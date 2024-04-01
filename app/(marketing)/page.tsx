988948d9af207ed9c2c6150de5713ba8d800e2b9
necr0z <necroz1998@gmail.com>
2024-04-01 15:03:24 +0800

    i was clever css fixed beforehand

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,6 +1,3 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
 import { Heading } from "./_components/heading";
 import { Heroes } from "./_components/heroes";
 import Footer from "./_components/footer";
@@ -8,6 +5,9 @@ import Footer from "./_components/footer";
 const MarketingPage = () => {
   return (
     // i need to dark mode here instead of layout.tsx. dk why
+
+    // glen chen from 3 weeks into the future. good job bro you fixed it before
+    // the creator himself knew nice la smileyface
     <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10 ">
         <Heading />
